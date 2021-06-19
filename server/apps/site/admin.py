c4e8a7d2a6371cbc9ba68b37cf745403056ec02f
saper663_home <saper663ru@gmail.com>
2021-06-19 21:04:59 +0500

    add add blank parameter to id_husband and id_wife

--- server/apps/site/admin.py
+++ server/apps/site/admin.py
@@ -4,10 +4,13 @@ from server.apps.site.models import Family, Person
 
 
 @admin.register(Family)
-class BlogPostAdmin(admin.ModelAdmin[Family]):
+class FamilyAdmin(admin.ModelAdmin[Family]):
     """Admin panel example for ``BlogPost`` model."""
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('id_husband', 'id_wife')
 
 
 @admin.register(Person)
-class BlogPostAdmin(admin.ModelAdmin[Person]):
+class PersonAdmin(admin.ModelAdmin[Person]):
     """Admin panel example for ``BlogPost`` model."""
