1f2ddf9a791ddb44b0c37e7f5c6b7abec8558bbb
SaPer663 <saper663ru@gmail.com>
2021-08-09 20:56:24 +0500

    added div in the html template to reduce the line indentation of the partner

--- server/apps/site/static/site/js/script.js
+++ server/apps/site/static/site/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
         $('.tab-label').click(function(){
             $(this).next().slideToggle(150);
+            $(this).next().next().slideToggle(150);
             $(this).find('i').toggleClass('bi bi-chevron-right').toggleClass('bi bi-chevron-down');
 
         })
